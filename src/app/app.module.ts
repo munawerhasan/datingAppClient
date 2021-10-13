ed41e16a534868df7989a88377913516301af821
munawerhasan5 <munawerhasan52@gmail.com>
2021-10-13 22:31:50 +0530

    deleted cd Com

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CdComponent } from './cd/cd.component';
-import { Cd1Component } from './cd1/cd1.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    CdComponent,
-    Cd1Component
   ],
   imports: [
     BrowserModule
