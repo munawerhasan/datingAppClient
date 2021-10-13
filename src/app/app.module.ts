1ae3593fda50e0d3ae389df79a39a23afb655269
munawerhasan5 <munawerhasan52@gmail.com>
2021-10-13 22:14:38 +0530

    fitstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CdComponent } from './cd/cd.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CdComponent
   ],
   imports: [
     BrowserModule
